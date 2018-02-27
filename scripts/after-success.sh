9b90d43fc8d7353b260bc6a39cbc0986aa0a0ab2
rithu john <rithujohn191@gmail.com>
2018-02-27 15:25:57 -0800

    scripts: modify script to push kube-etcd-signer-server and kube-client-agent images

--- scripts/after-success.sh
+++ scripts/after-success.sh
@@ -3,4 +3,6 @@
 if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
     docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD" quay.io;
     make push/kube-aws-approver;
+    make push/kube-etcd-signer-server;
+    make push/kube-client-agent;
 fi
