7732233fdf29a6571e8ae03da91e92a7c62a137a
Sam Batschelet <sbatsche@redhat.com>
2019-04-03 09:35:55 -0400

    *: add --max-retry flag to kube-client-agent
    Signed-off-by: Sam Batschelet <sbatsche@redhat.com>

--- pkg/certagent/agent.go
+++ pkg/certagent/agent.go
@@ -34,6 +34,9 @@ type CSRConfig struct {
 	// AssetsDir is the directory location where certificates and
 	// private keys will be saved
 	AssetsDir string `json:"assetsDir"`
+
+	// MaxRetry is the maximum attepts to retry client request on failure.
+	MaxRetry int `json:"maxRetry"`
 }
 
 // CertAgent is the top level object that represents a certificate agent.
@@ -108,7 +111,8 @@ func GenerateCSRObject(config CSRConfig) (*capi.CertificateSigningRequest, error
 
 // RequestCertificate will create a certificate signing request for a node
 // with the config given and send it to a signer via a POST request.
-// If something goes wrong it returns an error.
+// If something goes wrong it returns an error unless CSRConfig.MaxRetry
+// is defined in which case we will retry the request.
 // NOTE: This method does not return the approved CSR from the signer.
 func (c *CertAgent) RequestCertificate() error {
 	csr, err := GenerateCSRObject(c.config)
@@ -116,9 +120,28 @@ func (c *CertAgent) RequestCertificate() error {
 		return fmt.Errorf("error generating CSR Object: %v", err)
 	}
 
-	// send CSR to the signer
-	if _, err := c.client.Create(csr); err != nil {
-		return fmt.Errorf("error sending CSR to signer: %v", err)
+	backoff := wait.Backoff{
+		Steps:    c.config.MaxRetry,
+		Duration: 10 * time.Second,
+		Factor:   0.0,
+	}
+
+	var lastErr error
+	// implement a retry mechanism in the case request fails.
+	errc := wait.ExponentialBackoff(backoff, func() (bool, error) {
+		// send CSR to the signer
+		_, err := c.client.Create(csr)
+		if err != nil {
+			lastErr = err
+			return false, nil
+		}
+		return true, nil
+	})
+	if errc != nil {
+		if errc == wait.ErrWaitTimeout && lastErr != nil {
+			errc = lastErr
+		}
+		return fmt.Errorf("error sending CSR to signer: %v", errc)
 	}
 
 	rcvdCSR, err := c.WaitForCertificate()
